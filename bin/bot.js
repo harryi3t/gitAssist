d5a27f12bb542c32e796c88a3028421c1c46cc38
Harendra Singh <harendra@shippable.com>
2015-11-04 23:56:12 +0530

    Update bot.js
    To try keep the app running on heroku, so what we wont get the error

--- bin/bot.js
+++ bin/bot.js
@@ -8,4 +8,15 @@ var slackAssistBot = new SlackAssistBot({
   token: token
 });
 
+// Just to keep the app running on heroku
+var http = require('http');
+var port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, "127.0.0.1");
+
+console.log('Server running');
+
 slackAssistBot.run();
