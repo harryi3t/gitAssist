f2ffed22fed02925ae63e7d38c274cb5e567a2c0
Harendra Singh <harendra@shippable.com>
2015-11-05 00:01:43 +0530

    Update bot.js
    Removed hard coded IP address

--- bin/bot.js
+++ bin/bot.js
@@ -15,7 +15,7 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(port, "127.0.0.1");
+}).listen(port);
 
 console.log('Server running');
 
