583f891ed831149e43f2cd3094018257d11d397e
harryi3t <66hari@gmail.com>
2015-11-07 14:52:41 +0530

    Added token and repositories field in the collection accounts

--- api/accounts/Model.js
+++ api/accounts/Model.js
@@ -1,7 +1,12 @@
 'use strict';
 
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
 
-module.exports = mongoose.model('githubModel',{
-  url: { type: String, required: true }
+module.exports = mongoose.model('githubModel', {
+  url: {
+    type: String,
+    required: true
+  },
+  token: String,
+  repositories: [String]
 });
