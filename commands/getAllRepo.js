e322b87108f273cea015b148e7f09c5ac04b9d15
harryi3t <66hari@gmail.com>
2015-11-20 03:08:32 +0530

    Added command for listing repositories

---
+++ commands/getAllRepo.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var getAccount = require('../api/accounts/get.js');
+
+module.exports.pattern = /^get all (repos|repositories)$/i;
+module.exports.command  = 'get all repos|repositories';
+module.exports.run = run;
+
+function run(commandText,callback){
+
+  _getRepos(function(err,repos){
+    if(err)
+      var replyMessageText = 'Error while getting the repositories: ' + err;
+    else  
+      var replyMessageText = 'Your repositories are: `' + repos + '`';
+    callback(replyMessageText);
+  });
+}
+
+function _getRepos(callback){
+  getAccount(function(err, data) {
+    if (err) 
+      callback(err);
+    else{
+      var repoList = '';
+      data.repositories.forEach(function(repo){
+        repoList += '\n' + repo;
+      });
+      callback(null,repoList);
+    }
+  });
+}
